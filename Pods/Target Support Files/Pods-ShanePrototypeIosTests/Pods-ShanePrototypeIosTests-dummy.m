e60a7077151d80ad80a0ed6f206fae4fe502f314
Shane <shaneRudolfWorktive@gmail.com>
2016-11-09 21:27:40 -0800

    Add Realm

---
+++ Pods/Target Support Files/Pods-ShanePrototypeIosTests/Pods-ShanePrototypeIosTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShanePrototypeIosTests : NSObject
+@end
+@implementation PodsDummy_Pods_ShanePrototypeIosTests
+@end
---
+++ ShanePrototypeIos/controllers/RealmPrototypeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RealmPrototypeViewController.swift
+//  ShanePrototypeIos
+//
+//  Created by Shane on 11/9/16.
+//  Copyright © 2016 Shane. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RealmPrototyeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func saveDogs(sender: UIButton) {
+    }
+    
+    @IBAction func readDogs(sender: AnyObject) {
+    }
+}
